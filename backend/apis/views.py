66408558e905ce8dae894886eba2513a527546ab
arvindh07 <arvindhganesh11@gmail.com>
2024-06-18 21:12:18 +0530

    added register view

--- backend/apis/views.py
+++ backend/apis/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from django.contrib.auth.models import User
+from .models import UserSerializer
+from rest_framework.permissions import AllowAny
 # Create your views here.
+class RegisterView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
