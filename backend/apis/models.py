136ce00ae757a51ab481e9c96491821e5b5c884d
arvindh07 <arvindhganesh11@gmail.com>
2024-06-21 09:17:39 +0530

    added created_at and author field to note model

--- backend/apis/models.py
+++ backend/apis/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-from rest_framework import serializers
 # Create your models here.
 
 class Note(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
