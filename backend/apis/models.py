5ad8d0c80438abac00967c7a184a6ce58830ebfe
arvindh07 <arvindhganesh11@gmail.com>
2024-06-21 07:12:54 +0530

    created note model

--- backend/apis/models.py
+++ backend/apis/models.py
@@ -3,3 +3,7 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 # Create your models here.
 
+class Note(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
