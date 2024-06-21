e6a50190f47e1f64242fcfed8bc7329b7a3d80ad
arvindh07 <arvindhganesh11@gmail.com>
2024-06-21 07:30:14 +0530

    added view and url for notes

--- backend/apis/urls.py
+++ backend/apis/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView
+from .views import RegisterView, NoteView
 urlpatterns = [
     path("user/register", RegisterView.as_view(), name="register"),
-    path("token/", TokenObtainPairView.as_view(), name="token"),
-    path("token/refresh", TokenRefreshView.as_view(), name="token-refresh")
+    path("token", TokenObtainPairView.as_view(), name="token"),
+    path("token/refresh", TokenRefreshView.as_view(), name="token-refresh"),
+    path("note", NoteView.as_view(), name="note")
 ]
--- backend/apis/views.py
+++ backend/apis/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from rest_framework import generics
 from django.contrib.auth.models import User
-from .serializers import UserSerializer
-from rest_framework.permissions import AllowAny
+from .serializers import UserSerializer, NoteSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from .models import Note
 # Create your views here.
 class RegisterView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+class NoteView(generics.ListCreateAPIView):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
