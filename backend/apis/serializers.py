f1fd869dd191448aadc754786506f08aa8dc1bcc
arvindh07 <arvindhganesh11@gmail.com>
2024-06-21 07:16:08 +0530

    added note serializer

--- backend/apis/serializers.py
+++ backend/apis/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from .models import Note
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -9,3 +10,11 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+    
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = "__all__"
+
+    def __str__(self):
+        return f"{self.title} - {self.completed}"
