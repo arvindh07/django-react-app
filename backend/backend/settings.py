540ebeb4b208bbcf284be19a699ea5b0bbabcd95
arvindh07 <arvindhganesh11@gmail.com>
2024-06-21 16:48:39 +0530

    changed token timings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -35,8 +35,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=120),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1)
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(hours=2)
 }
 
 
